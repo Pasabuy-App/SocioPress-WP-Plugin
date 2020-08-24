8adef7d3e2c3d2e971aa4961dc644c6532ccacd6
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-24 17:16:18 +0800

    Modified insert activities.

--- includes/api/v1/activity/class-insert.php
+++ includes/api/v1/activity/class-insert.php
@@ -10,6 +10,7 @@
 		* This is the primary gateway of all the rest api request.
 	*/
   	class SP_Insert_Activity {
+
           public static function listen(){
             return rest_ensure_response( 
                 SP_Insert_Activity::insert_activty()
@@ -27,7 +28,7 @@
             $table_activity = SP_ACTIVITY_TABLE;
             $table_activity_fields = SP_ACTIVITY_FIELDS;
 
-            // Step1 : Check if prerequisites plugin are missing
+            // Step 1: Check if prerequisites plugin are missing
             $plugin = SP_Globals::verify_prerequisites();
             if ($plugin !== true) {
 
@@ -37,7 +38,7 @@
                 );
             }
 
-			//  Step2 : Validate if user is exist
+			//  Step 2: Validate user
 			if (DV_Verification::is_verified() == false) {
                 
                 return array(
@@ -46,6 +47,7 @@
                 );
             }
 
+            // Step 3: Check if required parameters are passed
             if (!isset($_POST['title']) || !isset($_POST['info']) || !isset($_POST['icon'])  ) {
                 return array(
                     "status" => "unknown",
@@ -53,6 +55,7 @@
                 );
             }
 
+            // Step 4: Check if parameters passed are empty
             if (empty($_POST['title']) || empty($_POST['info']) || empty($_POST['icon'])  ) {
                 return array(
                     "status" => "failed",
@@ -60,6 +63,7 @@
                 );
             }
 
+            // Step 5: Check if parameters are valid
             if ($_POST['icon'] != 'warn' && $_POST['icon'] != 'info' && $_POST['icon'] != 'error' ) {
                 return array(
                     "status" => "failed",
@@ -68,60 +72,53 @@
             }
 
             $user = SP_Insert_Activity::catch_post();
-
-            $user_date = TP_Globals::get_user_date($user["user_id"]);
+            $date = SP_Globals::date_stamp();
             $stid = 0;
             $wpid = 0;
-
+            
+            // Step 6: Check if user or store
             if(!isset($_POST['stid'])){
-            
                 $wpid = $user['user_id'];
-            
             }else{
-            
                 if ( !is_numeric($_POST['stid']) ) {
                     return array(
                         "status" => "failed",
                         "message" => "ID is not in valid format.",
                     );
                 }
-
                 if (empty($_POST['stid']) ) {
                     return array(
                         "status" => "failed",
                         "message" => "Required fields cannot be empty.",
                     );
                 }
-
                 $stid = $_POST['stid'];
-
             }
-
+            $created_by = $user['user_id'];
+            
+            // Step 7: Query
             $wpdb->query("START TRANSACTION");
 
-                $wpdb->query("INSERT INTO $table_revision $table_revision_fields VALUES ('activity', 0, 'title', '{$user["activity_title"]}', '{$user["user_id"]}', '$user_date' ) ");
+                $wpdb->query("INSERT INTO $table_revision $table_revision_fields VALUES ('activity', 0, 'title', '{$user["activity_title"]}', '{$user["user_id"]}', '$date' ) ");
                 $title_last_id = $wpdb->insert_id;
 
-                $wpdb->query("INSERT INTO $table_revision $table_revision_fields VALUES ('activity', 0, 'info', '{$user["activity_info"]}', '{$user["user_id"]}', '$user_date' ) ");
+                $wpdb->query("INSERT INTO $table_revision $table_revision_fields VALUES ('activity', 0, 'info', '{$user["activity_info"]}', '{$user["user_id"]}', '$date' ) ");
                 $info_last_id = $wpdb->insert_id;
 
-                $wpdb->query("INSERT INTO $table_activity $table_activity_fields VALUES ('$wpid', '$stid', '{$user["activity_icon"]}', '$title_last_id', '$info_last_id', NULL, '$user_date' ) ");
+                $wpdb->query("INSERT INTO $table_activity $table_activity_fields VALUES ('$wpid', '$stid', '{$user["activity_icon"]}', '$title_last_id', '$info_last_id', '$created_by', null, '$date' ) ");
                 $activity_last_id = $wpdb->insert_id;
 
                 $update_parent_id = $wpdb->query("UPDATE $table_revision SET `parent_id` = $activity_last_id WHERE ID IN ($title_last_id, $info_last_id) ");
-
             
-            if ($title_last_id < 1 || $info_last_id < 1 || $update_parent_id < 1) {
+            // Step 8: Check result
+            if ($title_last_id < 1 || $info_last_id < 1 || $activity_last_id < 1 || $update_parent_id < 1) {
                 $wpdb->query("ROLL BACK");
-
                 return array(
                     "status" => "error",
                     "message" => "An error occured while submitting data to server."
                 );
-
             }else{
                 $wpdb->query("COMMIT");
-
                 return array(
                     "status" => "success",
                     "message" => "Data has been submitted successfully."
@@ -133,8 +130,8 @@
         public static function catch_post(){
 
             $cur_user = array();
+
             $cur_user['user_id'] = $_POST['wpid'];
-            
             $cur_user['activity_icon']  = $_POST['icon'];
             $cur_user['activity_title'] = $_POST['title'];
             $cur_user['activity_info']  = $_POST['info'];
