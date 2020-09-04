b38f9bcd9de509f5166e837703331a6459785bb4
Miguel Radaza <48942632+MiguelRadaza@users.noreply.github.com>
2020-09-04 10:25:31 +0800

    Added new results in listing data

--- includes/api/v1/profile/class-data.php
+++ includes/api/v1/profile/class-data.php
@@ -13,13 +13,14 @@
 
         public static function listen(){
             return rest_ensure_response( 
-                SP_Profile_data::get_profile_data()
+                self::get_profile_data()
             );
     
         }
          
         // REST API for getting the user data
         public static function get_profile_data(){
+            global $wpdb;
 
             // Step 1: Check if prerequisites plugin are missing
             $plugin = SP_Globals::verify_prerequisites();
@@ -40,6 +41,35 @@
 
             // Step 3: Find user in db using wpid
             $wp_user = get_user_by("ID", $_POST['wpid']);
+            $wpid = $_POST['wpid'];
+
+            $user_address = $wpdb->get_row("SELECT
+                `add`.ID,
+                `add`.stid,
+                IF(`add`.types = 'business', 'Business', 'Office' )as `type`,
+                ( SELECT child_val FROM dv_revisions WHERE id = `add`.street ) AS street,
+                ( SELECT brgy_name FROM dv_geo_brgys WHERE ID = ( SELECT child_val FROM dv_revisions WHERE id = `add`.brgy ) ) AS brgy,
+                ( SELECT city_name FROM dv_geo_cities WHERE city_code = ( SELECT child_val FROM dv_revisions WHERE id = `add`.city ) ) AS city,
+                ( SELECT prov_name FROM dv_geo_provinces WHERE prov_code = ( SELECT child_val FROM dv_revisions WHERE id = `add`.province ) ) AS province,
+                ( SELECT country_name FROM dv_geo_countries WHERE id = ( SELECT child_val FROM dv_revisions WHERE id = `add`.country ) ) AS country,
+                IF (( select child_val from dv_revisions where id = `add`.`status` ) = 1, 'Active' , 'Inactive' ) AS `status`,
+                `add`.date_created
+            FROM
+                dv_address `add`
+            WHERE wpid = $wpid"); 
+
+            $user_contact = $wpdb->get_results("SELECT
+                -- dc.ID,
+                -- dc.stid,
+                -- IF ( dc.`status` = 1, 'Active', 'Inactive' ) as `status`,
+                -- dc.types,
+                dr.child_val AS `value`
+                -- dc.date_created 
+            FROM
+                dv_contacts dc
+                INNER JOIN dv_revisions dr ON dr.ID = dc.revs 
+            WHERE
+                dc.`wpid` = '$wpid'");
                     
             // Step 4: Return success status and complete object.
             return array(
@@ -48,10 +78,16 @@
                         "uname" => $wp_user->data->user_nicename,
                         "dname" => $wp_user->data->display_name,
                         "email" => $wp_user->data->user_email,
-                        "ro" => $wp_user->roles,
-                        "fn" => $wp_user->first_name,
-                        "ln" => $wp_user->last_name,
-                        "av" => $wp_user->avatar
+                        "role" => $wp_user->roles,
+                        "date_registered" => $wp_user->user_registered,
+                        "fname" => $wp_user->first_name,
+                        "lname" => $wp_user->last_name,
+                        "avatar" => $wp_user->avatar,
+                        "contact" => $user_contact[0]->value,
+                        "street" => $user_address->street,
+                        "brgy"  => $user_address->brgy,
+                        "city"  => $user_address->city,
+                        "prov"  => $user_address->province,
                     )
             );
         }// End of function initialize()
