ae872877557b2e869a29c9c36c908634d52a1f64
Miguel Radaza <miguel1.radaza@gmail.com>
2020-09-07 19:45:34 +0800

    Add new condition in inserting post

--- includes/api/v1/post/class-insert.php
+++ includes/api/v1/post/class-insert.php
@@ -4,7 +4,7 @@
 		exit;
 	}
 
-	/** 
+	/**
         * @package sociopress-wp-plugin
 		* @version 0.1.0
 		* This is the primary gateway of all the rest api request.
@@ -12,18 +12,18 @@
   	class SP_Insert_Post {
 
         public static function listen(){
-            return rest_ensure_response( 
+            return rest_ensure_response(
                 SP_Insert_Post:: list_open()
             );
         }
-         
+
         public static function list_open(){
-            
+
             // Initialize WP global variable
 			global $wpdb;
-            
+
             $user = SP_Insert_Post::catch_post();
-			
+
             // Step 1: Check if prerequisites plugin are missing
             $plugin = SP_Globals::verify_prerequisites();
             if ($plugin !== true) {
@@ -32,7 +32,7 @@
                     "message" => "Please contact your administrator. ".$plugin." plugin missing!",
                 );
             }
-            
+
             // Step 2: Validate user
             if (DV_Verification::is_verified() == false) {
                 return array(
@@ -46,7 +46,7 @@
 				return array(
 					"status" => "unknown",
 					"message" => "Please contact your administrator. Request unknown!",
-                ); 
+                );
             }
 
             // Step 4: Check if parameters passed are empty
@@ -64,20 +64,21 @@
                     "message" => "Invalid post type.",
                 );
             }
-			
+
 			$insert_post = array(
-				'post_author'	=>$user["created_by"],
-				'post_title'	=>$user["title"], 
-				'post_content'	=>$user["content"], 
-				'post_status'	=>$user["post_status"], 
-				'comment_status'=>$user["comment_status"],
-				'ping_status'	=>$user["ping_status"], 
-				'post_type'		=>$user["post_type"]
+				'post_author'	=> $user["created_by"],
+				'post_title'	=> $user["title"],
+				'post_content'	=> $user["content"],
+				'post_status'	=> $user["post_status"],
+				'comment_status'=> $user["comment_status"],
+				'ping_status'	=> $user["ping_status"],
+				'post_type'		=> $user["post_type"]
 			);
 
             // Step 6: Start mysql transaction
-			$result = wp_insert_post($insert_post);
-			
+            $result = wp_insert_post($insert_post);
+            update_post_meta($result,awd );
+
             // Step 7: Check if any queries above failed
             if ($result < 1) {
                 return array(
@@ -92,12 +93,12 @@
                 "message" => "Data has been added successfully.",
             );
 		}
-		
-        // Catch Post 
+
+        // Catch Post
         public static function catch_post()
         {
               $cur_user = array();
-               
+
                 $cur_user['created_by']     = $_POST["wpid"];
                 $cur_user['title']          = $_POST["title"];
                 $cur_user['content']        = $_POST["content"];
@@ -105,7 +106,7 @@
                 $cur_user['comment_status'] = 'open';
                 $cur_user['ping_status']    = 'open';
                 $cur_user['post_type']      = $_POST["type"];
-  
+
               return  $cur_user;
         }
     }
