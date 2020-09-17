9904fd8b93f6ed3649d05e42e49515dc48275119
Miguel Radaza <miguel1.radaza@gmail.com>
2020-09-17 22:20:13 +0800

    Remove unessesarry comment

--- includes/api/v1/messages/class-getby-recepient.php
+++ includes/api/v1/messages/class-getby-recepient.php
@@ -33,12 +33,12 @@
             }
 
 			// Step 2: Validate user
-			/* if (DV_Verification::is_verified() == false) {
+			if (DV_Verification::is_verified() == false) {
                 return array(
                     "status"  => "unknown",
                     "message" => "Please contact your administrator. Verification issues!",
                 );
-			} */
+			}
 
 			$wpid = $_POST['wpid'];
 			$user_id = $_POST['recepient'];
