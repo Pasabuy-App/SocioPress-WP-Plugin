c5b5014944f422a74b7830285541c0cf914f8d48
Miguel Radaza <miguel1.radaza@gmail.com>
2020-10-30 17:23:25 +0800

    Fixed bugs in sociopress

--- includes/api/v1/feeds/class-home.php
+++ includes/api/v1/feeds/class-home.php
@@ -36,12 +36,12 @@
 
 
 			// Step 2: Validate user
-			// if (DV_Verification::is_verified() == false) {
-			//  	return array(
-			// 		"status" => "unknown",
-			// 		"message" => "Please contact your administrator. Verification issues!",
-			//  	);
-			// }
+			if (DV_Verification::is_verified() == false) {
+			 	return array(
+					"status" => "unknown",
+					"message" => "Please contact your administrator. Verification issues!",
+			 	);
+			}
 
 			// Step 3: Start mysql transaction
 			$sql = "SELECT
